5cbf8e49abe8108f8f5249bcea449c7ecf6f8cc5
Damian <52243518+damianjude@users.noreply.github.com>
2026-02-07 18:26:28 +0530

    Fix dockerignore and include bin, also add hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Allow requests routed through the Nginx upstream name.
+  config.hosts << "app"
+
   # Enable server timing
   config.server_timing = true
 
